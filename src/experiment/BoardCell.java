7ea1a2aa419b9beab591fbe19f7f2378b23b1452
Liz Boyle <eboyle@mines.edu>
2017-10-04 16:03:09 -0600

    Methods laid out

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -1,5 +1,14 @@
 package experiment;
 
 public class BoardCell {
-
+	private int yCoord;
+	private int xCoord;
+	
+	public BoardCell(int yCoord, int xCoord) {
+		super();
+		this.yCoord = yCoord;
+		this.xCoord = xCoord;
+	}
+	
+	
 }
